db6baa778c43456b9cf84190def76c2a38e7258f
dugulanhun <dugulanhun@163.com>
2022-06-21 16:04:15 +0800

    Shell Physics

--- Source/Blaster/Weapon/Casing.cpp
+++ Source/Blaster/Weapon/Casing.cpp
@@ -2,6 +2,8 @@
 
 
 #include "Casing.h"
+#include "Kismet/GameplayStatics.h"
+#include "Sound/SoundCue.h"
 
 ACasing::ACasing()
 {
@@ -9,6 +11,12 @@ ACasing::ACasing()
 
 	CasingMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("CasingMesh"));
 	SetRootComponent(CasingMesh);
+	CasingMesh->SetCollisionResponseToChannel(ECollisionChannel::ECC_Camera, ECollisionResponse::ECR_Ignore);
+	CasingMesh->SetSimulatePhysics(true);
+	CasingMesh->SetEnableGravity(true);
+	CasingMesh->SetNotifyRigidBodyCollision(true);
+	// 子弹弹射速度
+	ShellEjectionImpulse = 10.f;
 
 }
 
@@ -16,6 +24,19 @@ void ACasing::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	CasingMesh->OnComponentHit.AddDynamic(this, &ACasing::OnHit);
+	CasingMesh->AddImpulse(GetActorForwardVector() * ShellEjectionImpulse);
+
+	// 设置生命周期为10秒
+	SetLifeSpan(10);
 }
 
+void ACasing::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	if (ShellSound)
+	{
+		UGameplayStatics::PlaySoundAtLocation(this, ShellSound, GetActorLocation());
+	}
+	// Destroy();
+}
 
