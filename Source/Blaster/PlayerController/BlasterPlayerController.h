4a3a1bfd058a185349cb205db9212c8c32f71712
dugulanhun <dugulanhun@163.com>
2022-06-29 19:37:08 +0800

    Syncing Client and Server Time

--- Source/Blaster/PlayerController/BlasterPlayerController.h
+++ Source/Blaster/PlayerController/BlasterPlayerController.h
@@ -19,10 +19,40 @@ public:
 	void SetHUDDefeats(int32 Defeats);
 	void SetHUDWeaponAmmo(int32 Ammo);
 	void SetHUDCarriedAmmo(int32 Ammo);
+	void SetHUDMatchCountdown(float CountdownTime);		// 设置HUD的
 	virtual void OnPossess(APawn* InPawn) override;
+	virtual void Tick(float DeltaTime) override;
+
+	virtual float GetServerTime(); // Synced with server world clock
+	virtual void ReceivedPlayer() override; // Sync with server clock as soon as possible
 protected:
 	virtual void BeginPlay() override;
+
+	void SetHUDTime();			// 控制HUD更新频率方式
 private:
 	UPROPERTY()
 	class ABlasterHUD* BlasterHUD;
+
+	float MatchTime = 120.f;		// Temp
+	uint32 CountdownInt = 0;		// 计数，控制整数秒才更新HUD
+
+	/**
+	* Sync time between client and server
+	*/
+
+	// Requests the current server time, passing in the client's time when the request was sent
+	UFUNCTION(Server, Reliable)
+	void ServerRequestServerTime(float TimeOfClientRequest);
+
+	// Reports the current server time to the client in response to ServerRequestServerTime
+	UFUNCTION(Client, Reliable)
+	void ClientReportServerTime(float TimeOfClientRequest, float TimeServerReceivedClientRequest);
+
+	float ClientServerDelta = 0.f;			// difference between client and server time
+
+	UPROPERTY(EditAnywhere, Category = Time)
+	float TimeSyncFrequency = 5.f;			// 同步频率（默认5秒同步一次，可在蓝图中修改）
+
+	float TimeSyncRunningTime = 0.f;		// 记录每次同步后运行的时间
+	void CheckTimeSync(float DeltaTime);	// 同步函数
 };
