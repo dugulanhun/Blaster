93040e7d6eab56ddf4f83caad37e60b59f63d6fb
dugulanhun <dugulanhun@163.com>
2022-07-03 08:29:44 +0800

    Shotgun

--- Source/Blaster/Weapon/HitScanWeapon.cpp
+++ Source/Blaster/Weapon/HitScanWeapon.cpp
@@ -8,6 +8,10 @@
 #include "Kismet/GameplayStatics.h"
 #include "particles/ParticleSystemComponent.h"
 #include "Sound/SoundCue.h"
+#include "Kismet/KismetMathLibrary.h"
+#include "WeaponTypes.h"
+
+#include "DrawDebugHelpers.h"
 
 void AHitScanWeapon::Fire(const FVector& HitTarget)
 {
@@ -105,4 +109,25 @@ void AHitScanWeapon::Fire(const FVector& HitTarget)
 		}
 	}
 
+}
+
+FVector AHitScanWeapon::TraceEndWithScatter(const FVector& TraceStart, const FVector& HitTarget)
+{
+	FVector ToTargetNormalized = (HitTarget - TraceStart).GetSafeNormal();		// 获得射击方向单位向量
+	FVector SphereCenter = TraceStart + ToTargetNormalized * DistanceToSphere;
+	FVector RandVec = UKismetMathLibrary::RandomUnitVector() * FMath::FRandRange(0.f, SphereRadius);
+	FVector EndLoc = SphereCenter + RandVec;
+	FVector ToEndLoc = EndLoc - TraceStart;
+
+	DrawDebugSphere(GetWorld(), SphereCenter, SphereRadius, 12, FColor::Red, true);
+	DrawDebugSphere(GetWorld(), EndLoc, 4.f, 12, FColor::Orange, true);
+	DrawDebugLine(
+		GetWorld(),
+		TraceStart,
+		FVector(TraceStart + ToEndLoc / ToEndLoc.Size() * TRACE_LENGTH),
+		FColor::Cyan,
+		true
+	);
+
+	return FVector(TraceStart + ToEndLoc / ToEndLoc.Size() * TRACE_LENGTH);
 }
