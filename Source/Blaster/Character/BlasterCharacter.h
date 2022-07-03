5b60fd43c2f5b024b2c2ba77c37a6a359a95a449
dugulanhun <dugulanhun@163.com>
2022-07-03 22:58:45 +0800

    Sniper Scope

--- Source/Blaster/Character/BlasterCharacter.h
+++ Source/Blaster/Character/BlasterCharacter.h
@@ -50,6 +50,9 @@ public:
 	UPROPERTY(Replicated)
 	bool bDisableGameplay = false;
 
+	UFUNCTION(BlueprintImplementableEvent)
+	void ShowSniperScopeWidget(bool bShowScope);
+
 protected:
 	virtual void BeginPlay() override;
 
